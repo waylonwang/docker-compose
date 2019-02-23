47d50933a175430fddb97926eb530fe9f0dbf90d
waylon wang <waylonwang@users.noreply.github.com>
2019-02-23 17:35:05 +0800

    Update compose_vpn.sh

--- compose_vpn.sh
+++ compose_vpn.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker network create --driver bridge pub_vpn || true
+docker network inspect pub_vpn &>/dev/null || docker network create --driver bridge pub_vpn
 docker-compose -p vpn_service -f vpn.yml up -d
