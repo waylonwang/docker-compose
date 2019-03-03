deaafc4e5d4110ac54b2470bdeee3fd9c44e8041
waylon wang <waylonwang@users.noreply.github.com>
2019-03-04 00:40:25 +0800

    Update compose_db.sh

--- compose_db.sh
+++ compose_db.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
+export DB_HOST="db.cloudvalley.pw"
+
 docker network inspect pub_db &>/dev/null || docker network create --driver bridge pub_db
 docker-compose -p db_service -f db.yml up -d
