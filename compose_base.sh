ba07e90536e9b5169aeacb019cc519f5a0e704e9
waylon wang <waylonwang@users.noreply.github.com>
2019-04-11 17:21:55 +0800

    Update compose_base.sh

--- compose_base.sh
+++ compose_base.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
 
+source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/check_docker_env.sh)
+
 docker network inspect base &>/dev/null || docker network create --driver bridge base
-docker-compose -p base_service -f base.yml up -d
+
+if [ "$(get_os)" = "raspbian" ]; then
+  docker-compose -p base_service -f base_raspbian.yml up -d
+else
+  docker-compose -p base_service -f base.yml up -d
+fi
