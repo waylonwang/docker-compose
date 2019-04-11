425332e2649e92020c3a5a784fd8314ae9daa0eb
waylon wang <waylonwang@users.noreply.github.com>
2019-04-11 18:43:45 +0800

    Create compose_php.sh

---
+++ compose_php.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/check_docker_env.sh)
+
+docker network inspect base &>/dev/null || docker network create --driver bridge base
+
+if [ "$(get_os)" = "raspbian" ]; then
+  docker-compose -p base_service -f php_raspbian.yml up -d
+else
+  docker-compose -p base_service -f php.yml up -d
+fi
