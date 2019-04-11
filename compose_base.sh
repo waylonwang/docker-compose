f7eb30cb9ca82e279c632e079180bf9a31e055e6
waylon wang <waylonwang@users.noreply.github.com>
2019-04-11 18:00:50 +0800

    Update compose_base.sh

--- compose_base.sh
+++ compose_base.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 
-# 获取当前操作系统名称
-# 输入: 无
-# 输出: 操作系统名称 debian|ubuntu|devuan|centos|fedora|rhel|raspbian
-function get_os()
-{
-  source /etc/os-release
-  echo $ID
-}
+source <(curl -s https://raw.githubusercontent.com/waylonwang/sh-scripts/master/lib/check_docker_env.sh)
 
 docker network inspect base &>/dev/null || docker network create --driver bridge base
 
