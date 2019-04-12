3d83ef27d6236fc636b02f913ef25d856f8f3726
waylon <waylon.act@gmail.com>
2019-04-12 17:05:29 +0800

    split raspbian script

--- compose_base_raspbian.sh
+++ compose_base_raspbian.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker network inspect base &>/dev/null || docker network create --driver bridge base
-docker-compose -p base_service -f base.yml up -d
+docker-compose -p base_service -f base_raspbian.yml up -d
