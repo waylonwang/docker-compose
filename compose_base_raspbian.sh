49c94cdf14101bdd62f699a9040afece1a7f75e3
waylon wang <waylonwang@users.noreply.github.com>
2019-05-05 11:56:11 +0800

    Update compose_base_raspbian.sh

--- compose_base_raspbian.sh
+++ compose_base_raspbian.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker network inspect base &>/dev/null || docker network create --driver bridge base
+docker network inspect base > /dev/null 2>&1 || docker network create --driver bridge base
 docker-compose -p base_service -f base_raspbian.yml up -d
